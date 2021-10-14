8db533c0b061aa1c6a4a76b77a065664dc63858b
Michael Held <bodmbod@gmail.com>
2021-10-14 16:38:08 +0300

    enviroment and php roadmap

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head></head>
+	<body>
+
+
+		<h1 style="text-align: center;">Hi!</h1>
+		<div class="lernButton"><a href="/php/">Lern php</a></div>
+	</body>
+
+
+<style>
+	.lernButton {
+		border: 1px solid blue;
+		width: 200px;
+		height: 100px;
+		display: grid;
+		place-items: center;
+	}
+</style>
+
+</html>
---
+++ src/php/index.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<h1 style="text-align:center">PHP roadmap</h1>
+<ul>
+<li>Watch <a href="https://www.youtube.com/watch?v=2eebptXfEvw">tutorial</a> and repeat code in the directory <span class="code">tutorial</span> till database.
+<ul>
+<li> In <span class="code">tutorial</span> create a directory with name of lesson with <span class="code">index.php</span> file. </li> 
+<li>Check result in browser <span class="code">http://localhost:8000/php/tutorial/nameOfDirectory</span></li>
+<li>1 lesson = 1 or more commits. 1 commit != many lessons</li>
+</ul>
+</li>
+<li>
+Create account on <a href="https://www.codewars.com">Codewars</a> Make 10-20 tasks on PHP.
+<ul>
+	<li>In the directory <span class="code">codewars</span> create directory with name of task and index.php file.</li>
+	<li>In <span class="code">index.php</span> file must be description of task and one or more solutions</li>
+	<li>Show results. <span class="code">http://localhost:8000/php/codewars/nameOfDirectory</span>
+	<li>In same time read <a href="https://www.amazon.de/-/en/Aditya-Bhargava/dp/1617292230">the book</a> </li>
+</ul>
+</li>
+<li>Continue to watch and repeat the  <a href="https://www.youtube.com/watch?v=2eebptXfEvw">tutorial</a></li>
+</ul>
+<style>
+	body {
+		line-height: 150%;
+	}
+	li {
+		margin: 10px 0;
+	}
+	.code {
+		background-color: lightgrey;
+	}
+</style>
+</body> 
+</html>
