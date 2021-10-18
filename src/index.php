e888b022a49f4142db3d040a51bb87d616287c7f
Michael Held <bodmbod@gmail.com>
2021-10-19 00:40:19 +0200

    more imfo

--- src/index.php
+++ src/index.php
@@ -7,8 +7,11 @@
         <h1>Hi!</h1>
 </header>
         <div class="contentContainer"> 
-        <div class="lernButton"><a href="/php/">Lern php</a></div>
-    </div>
+        <div class="lernButton"><a href="/php/"><img src="/static/img/php.png" class="lernButtonImg"/></a><a href="/php/">Lern php</a></div>
+        <div class="lernButton"><a href="/symfony/"><img src="/static/img/symfony.png" class="lernButtonImg"/></a><a href="/symfony/">Lern Symfony</a></div>
+        <div class="lernButton"><a href="/js/"><img src="/static/img/js.png" class="lernButtonImg"/></a><a href="/js/">Lern js</a></div>
+        <div class="lernButton"><a href="/vue/"><img src="/static/img/vue.png" class="lernButtonImg"/></a><a href="/vue/">Lern vue</a></div>
+ </div>
 	</body>
 
 </html>
--- src/php/index.php
+++ src/php/index.php
@@ -8,23 +8,25 @@
 </header>
 <div class="contentContainer">
     <ul>
-        <li class="stepList">Watch <a href="https://www.youtube.com/watch?v=2eebptXfEvw">PHP tutorial</a> and repeat code in the directory <span class="code">tutorial</span> till <b>database</b>.
-            <ul>
+        <li class="stepList"><b>STEP 1:</b> Watch <a href="https://www.youtube.com/watch?v=2eebptXfEvw">PHP tutorial</a> and repeat code in the directory <span class="code">tutorial</span> till <b>DATABASE ( about 2:34:00) </b>. You have all enviroument and you don't need to install <b>XAMP</b>. Just VSCode or PHPStorm.
+            <h4 class="adviceHead">Some advice:</h4>
+            <ul> 
                 <li> In <span class="code">tutorial</span> create a directory with name of lesson with <span class="code">index.php</span> file. </li> 
                 <li>Check result in browser <span class="code">http://localhost:8000/php/tutorial/nameOfDirectory</span></li>
                 <li>1 lesson = 1 or more commits. 1 commit != many lessons</li>
             </ul>
         </li>
         <li class="stepList">
-            Create account on <a href="https://www.codewars.com">Codewars</a> Make 10-20 tasks on PHP.
+            <b>Step 2:</b> Create account on <a href="https://www.codewars.com">Codewars</a> Make 10-20 tasks on PHP. 
+            <h4 class="adviceHead">Some advice:</h4>
             <ul>
-	            <li>In the directory <span class="code">codewars</span> create directory with name of task and index.php file.</li>
+	            <li>In the directory <span class="code">codewars</span> create directory with name of task and <span class="code">index.php</span> file.</li>
 	            <li>In <span class="code">index.php</span> file must be description of task and one or more solutions</li>
 	            <li>Show results. <span class="code">http://localhost:8000/php/codewars/nameOfDirectory</span>
 	            <li>In same time read <a href="https://www.amazon.de/-/en/Aditya-Bhargava/dp/1617292230">the book (Grokking Algorithms)</a> </li>
             </ul>
         </li>
-        <li class="stepList">Continue to watch and repeat the  <a href="https://www.youtube.com/watch?v=2eebptXfEvw">PHP tutorial</a></li>
+        <li class="stepList">Continue to watch <a href="https://www.youtube.com/watch?v=2eebptXfEvw">PHP tutorial</a> and repeat code in directory <span class="code">tutorial</span></li>
     </ul>
 </div>
 </body> 
